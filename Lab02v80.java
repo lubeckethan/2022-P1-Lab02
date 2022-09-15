ee04e019bc663e6d6b81699f20d61dc11e080c07
11707 <ethan.lubeck@vtsd.com>
2022-09-15 08:15:30 -0400

    feiofhaeiofh

---
+++ Lab02v80.java
@@ -0,0 +1,28 @@
+/*
+    Welcome to your first lab!  All work should be completed inside of this file.
+    Please make sure that as you work on your lab you save, commit, and push everyday.
+
+    As always, if you have any questions reach out and email me!
+
+*/
+
+  public class Lab02v80 {
+
+    public static void main(String [] args)
+    {
+      System.out.println("Lab02, 80 Point Version");
+      System.out.println();
+      System.out.println();
+      System.out.print("Measure Today's Knowledge");
+      System.out.println("by yesterday's confusion");
+      System.out.println();
+      System.out.println("Bewilderment + Exposure = Obvious");
+      System.out.println();
+      System.out.println("# ##### #####  ##### ##### # #   # ##### #####");
+      System.out.println("# #   # #   #  #   # #   # # ##  #   #   #");
+      System.out.println("# #   # #   #  ##### #   # # # # #   #   #####");
+      System.out.println("# #   # #   #  #     #   # # #  ##   #       #");
+      System.out.println("# ##### #####  #     ##### # #   #   #   #####");
+
+    }
+  }
